1acf7508977e1cd179f5673d363b0da472649dd3
Geng Luo <geng.luo@student.kuleuven.be>
2024-05-26 20:36:02 +0200

    Remove all use of cuda

--- evaluate.py
+++ evaluate.py
@@ -26,14 +26,14 @@ import argparse
 from pprint import pprint
 
 import torch
-from zoedepth.utils.easydict import EasyDict as edict
+from metric_depth.zoedepth.utils.easydict import EasyDict as edict
 from tqdm import tqdm
 
-from zoedepth.data.data_mono import DepthDataLoader
-from zoedepth.models.builder import build_model
-from zoedepth.utils.arg_utils import parse_unknown
-from zoedepth.utils.config import change_dataset, get_config, ALL_EVAL_DATASETS, ALL_INDOOR, ALL_OUTDOOR
-from zoedepth.utils.misc import (RunningAverageDict, colors, compute_metrics,
+from metric_depth.zoedepth.data.data_mono import DepthDataLoader
+from metric_depth.zoedepth.models.builder import build_model
+from metric_depth.zoedepth.utils.arg_utils import parse_unknown
+from metric_depth.zoedepth.utils.config import change_dataset, get_config, ALL_EVAL_DATASETS, ALL_INDOOR, ALL_OUTDOOR
+from metric_depth.zoedepth.utils.misc import (RunningAverageDict, colors, compute_metrics,
                         count_parameters)
 
 
@@ -73,10 +73,12 @@ def evaluate(model, test_loader, config, round_vals=True, round_precision=3):
             if not sample['has_valid_depth']:
                 continue
         image, depth = sample['image'], sample['depth']
-        image, depth = image.cuda(), depth.cuda()
+        # image, depth = image.cuda(), depth.cuda()
         depth = depth.squeeze().unsqueeze(0).unsqueeze(0)
+        # focal = sample.get('focal', torch.Tensor(
+        #     [715.0873]).cuda())  # This magic number (focal) is only used for evaluating BTS model
         focal = sample.get('focal', torch.Tensor(
-            [715.0873]).cuda())  # This magic number (focal) is only used for evaluating BTS model
+            [715.0873]))  # This magic number (focal) is only used for evaluating BTS model
         pred = infer(model, image, dataset=sample['dataset'][0], focal=focal)
 
         # Save image, depth, pred for visualization
@@ -111,12 +113,29 @@ def evaluate(model, test_loader, config, round_vals=True, round_precision=3):
 def main(config):
     model = build_model(config)
     test_loader = DepthDataLoader(config, 'online_eval').data
-    model = model.cuda()
+    # model = model.cuda()
     metrics = evaluate(model, test_loader, config)
     print(f"{colors.fg.green}")
     print(metrics)
     print(f"{colors.reset}")
     metrics['#params'] = f"{round(count_parameters(model, include_all=True)/1e6, 2)}M"
+
+
+    import sys
+    output_file_name = "output_metrics.txt"
+
+    # Open the file in append mode
+    with open(output_file_name, 'a') as file:
+        # Write the metrics to the file
+        print(metrics, file=file)
+        
+        metrics['#params'] = f"{round(count_parameters(model, include_all=True)/1e6, 2)}M"
+        print(metrics['#params'], file=file)
+        
+        # Optionally, you can format the output to make it more readable
+        for key, value in metrics.items():
+            print(f"{key}: {value}", file=file)
+
     return metrics
 
 
